073b0f128d397acc9c8cc9848d663095ddbe3963
nyetii <netero725@gmail.com>
2022-10-29 02:15:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Kimi.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Kimi
+{
+    class Program
+    {
+        public static Task Main() => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appconfig.json")
+                .Build();
+
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                services
+                .AddSingleton(config)
+                .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    GatewayIntents = GatewayIntents.All,
+                    AlwaysDownloadUsers = false
+                }))
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>()
+                .AddSingleton(x => new CommandService())
+                .AddSingleton<PrefixHandler>())
+                .Build();
+
+            await RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            var _client = provider.GetRequiredService<DiscordSocketClient>();
+            var sCommands = provider.GetRequiredService<InteractionService>();
+            await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
+            var config = provider.GetRequiredService<IConfigurationRoot>();
+            var pCommands = provider.GetRequiredService<PrefixHandler>();
+            await pCommands.InitializeAsync();
+
+            pCommands.AddModule<PrefixModule>();
+
+            _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+            sCommands.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+
+            _client.Ready += async () =>
+            {
+                Console.WriteLine("Ready!");
+                await _client.SetGameAsync("joserobjr", null, ActivityType.Watching);
+                await _client.SetStatusAsync(UserStatus.Idle);
+                await sCommands.RegisterCommandsGloballyAsync();
+            };
+
+            await _client.LoginAsync(TokenType.Bot, config["Token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
