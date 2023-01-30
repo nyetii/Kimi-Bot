3896fb09bb0d796e336a93cbed6fe98e275f7faf
nyetii <netero725@gmail.com>
2023-01-29 21:22:17 -0300

    Shutdown command

---
+++ src/Kimi.Core/Modules/Utils/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Kimi.Core.Modules.Utils
+{
+    public class State : ModuleBase<SocketCommandContext>
+    {
+        [RequireOwner]
+        [Command("shutdown")]
+        public async Task Shutdown()
+        {
+            await ReplyAsync("Bye!");
+            Environment.Exit(0);
+        }
+    }
+}
